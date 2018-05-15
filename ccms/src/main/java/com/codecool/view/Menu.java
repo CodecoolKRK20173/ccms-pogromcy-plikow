49f3898443040ffd315205c608b23631ad811198
piotrkaminski95 <pkaminki95@gmail.com>
2018-05-15 19:22:04 +0200

    implement Menu

--- ccms/src/main/java/com/codecool/view/Menu.java
+++ ccms/src/main/java/com/codecool/view/Menu.java
@@ -1,4 +1,31 @@
 package com.codecool.view;
 
-public class Menu {
+public abstract class Menu {
+    private View view;
+    private UserInput userInput;
+    protected String[] options;
+    protected String label;
+
+    protected abstract void executeOption(int option);
+
+    public void run() {
+        boolean isRunning = true;
+        while (isRunning) {
+            view.print();
+            view.print(label);
+            view.print("0: quit");
+            view.printOptions(options);
+
+            view.print("Choose option:");
+            int userChoice = userInput.chooseOption();
+
+            if (userChoice == 0) {
+                isRunning = false;
+            } else {
+                executeOption(userChoice);
+                view.print();
+            }
+        }
+    }
 }
+
