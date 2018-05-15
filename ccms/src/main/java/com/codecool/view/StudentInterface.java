fc1ad6c77e121d0fa7be10172680e24145bff90d
piotrkaminski95 <pkaminki95@gmail.com>
2018-05-15 19:48:43 +0200

    implement StudentInterface

--- ccms/src/main/java/com/codecool/view/StudentInterface.java
+++ ccms/src/main/java/com/codecool/view/StudentInterface.java
@@ -1,4 +1,28 @@
 package com.codecool.view;
 
-public class StudentInterface {
+public class StudentInterface extends Menu {
+    public StudentInterface() {
+        this.label = "Student";
+        this.options = new String[] {
+                "View my grades",
+                "Submit assignment",
+        };
+    }
+
+    protected void executeOption(int option) {
+        switch (option) {
+            case 1: {
+                System.out.println("View my grades");
+                break;
+            }
+            case 2: {
+                System.out.println("Submit assignment");
+                break;
+            }
+            default: {
+                System.out.println("Wrong command");
+                break;
+            }
+        }
+    }
 }
