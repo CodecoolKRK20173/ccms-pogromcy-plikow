22b1a32b026a007acd4def6a5a2fb9b63925be9f
piotrkaminski95 <pkaminki95@gmail.com>
2018-05-15 19:05:20 +0200

    add print() printOptions()

--- ccms/src/main/java/com/codecool/view/View.java
+++ ccms/src/main/java/com/codecool/view/View.java
@@ -1,4 +1,20 @@
 package com.codecool.view;
 
 public class View {
+
+    public void print() {
+        System.out.println();
+    }
+
+    public void print(String s) {
+        System.out.println(s);
+    }
+
+    public void printOptions(String[] options) {
+        int index = 1;
+
+        for (String option: options) {
+            System.out.println(String.format("%d: %s", index, option));
+        }
+    }
 }
