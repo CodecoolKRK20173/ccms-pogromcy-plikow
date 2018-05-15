813767cb028c23255304a0d232a5e39af7a4a0fc
piotrkaminski95 <pkaminki95@gmail.com>
2018-05-15 19:57:08 +0200

    implement RegularEmployeeInterface

--- ccms/src/main/java/com/codecool/view/RegularEmployeeInterface.java
+++ ccms/src/main/java/com/codecool/view/RegularEmployeeInterface.java
@@ -1,4 +1,23 @@
 package com.codecool.view;
 
-public class RegularEmployeeInterface {
+public class RegularEmployeeInterface extends Menu{
+    public RegularEmployeeInterface() {
+        this.label = "Employee";
+        this.options = new String[] {
+                "List students",
+        };
+    }
+
+    protected void executeOption(int option) {
+        switch (option) {
+            case 1: {
+                System.out.println("List students");
+                break;
+            }
+            default: {
+                System.out.println("Wrong command");
+                break;
+            }
+        }
+    }
 }
