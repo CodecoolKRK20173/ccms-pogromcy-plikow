b70e0646983a9fffb63b202d2f3a279cf1fd2a7d
piotrkaminski95 <pkaminki95@gmail.com>
2018-05-15 21:59:04 +0200

    implement User

--- ccms/src/main/java/com/codecool/model/User.java
+++ ccms/src/main/java/com/codecool/model/User.java
@@ -1,4 +1,59 @@
 package com.codecool.model;
 
 public class User {
+    private String id;
+    private String password;
+    private String name;
+    private String surname;
+    private String eMail;
+    private String type;
+
+    public User(String id, String password, String name, String surname, String eMail, String type) {
+        this.id = id;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+        this.eMail = eMail;
+        this.type = type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
 }
