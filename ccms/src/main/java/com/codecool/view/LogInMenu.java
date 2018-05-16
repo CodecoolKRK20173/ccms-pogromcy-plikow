0ceef3f6a8b31596b4d5daa6f8c81636ea67dd9f
piotrkaminski95 <pkaminki95@gmail.com>
2018-05-16 19:00:32 +0200

    add and implement LogInMenu

---
+++ ccms/src/main/java/com/codecool/view/LogInMenu.java
@@ -0,0 +1,48 @@
+package com.codecool.view;
+
+public class LogInMenu extends Menu {
+    private Menu menu;
+
+    public LogInMenu() {
+        this.label = "Log in:";
+        this.options = new String[] {
+            "Log in as manager",
+            "Log in as mentor",
+            "Log in as student",
+            "Log in as regular employee"
+        };
+    }
+
+    protected void executeOption(int option) {
+        switch (option) {
+            case 1: {
+                menu = new ManagerMenu();
+                menu.run();
+                break;
+            }
+
+            case 2: {
+                menu = new MentorMenu();
+                menu.run();
+                break;
+            }
+
+            case 3: {
+                menu = new StudentMenu();
+                menu.run();
+                break;
+            }
+
+            case 4: {
+                menu = new RegularEmployeeMenu();
+                menu.run();
+                break;
+            }
+
+            default: {
+                System.out.println("Wrong command");
+                break;
+            }
+        }
+    }
+}
