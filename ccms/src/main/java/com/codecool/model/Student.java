46361cc638cc57ca64267a92bcd4aaad818967fe
piotrkaminski95 <pkaminki95@gmail.com>
2018-05-16 13:16:43 +0200

    implement getDoneAssigments()

--- ccms/src/main/java/com/codecool/model/Student.java
+++ ccms/src/main/java/com/codecool/model/Student.java
@@ -1,4 +1,35 @@
 package com.codecool.model;
 
-public class Student {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student extends User{
+    final static String type = "student";
+    private List<Assignment> assigments;
+
+    public Student(String id, String password, String name, String surname, String eMail) {
+        super(id, password, name, surname, eMail, type);
+        assigments = new ArrayList<Assignment>();
+    }
+
+    public String[] getContactData() {
+        String[] result = new String[] {
+                this.getName(),
+                this.getSurname(),
+                this.geteMail()
+        };
+        return result;
+    }
+
+    public List<Assignment> getDoneAssigment() {
+        List<Assignment> doneAssigments = new ArrayList<Assignment>();
+
+        for (Assignment assignment: assigments) {
+            if (assignment.getStatus()) {
+                doneAssigments.add(assignment);
+            }
+        }
+
+        return doneAssigments;
+    }
 }
