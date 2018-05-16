bebaeb9e1d0eed1100bf80e1248b0877264b5016
piotrkaminski95 <pkaminki95@gmail.com>
2018-05-16 19:01:57 +0200

    implement LogInController

--- ccms/src/main/java/com/codecool/controller/LogInController.java
+++ ccms/src/main/java/com/codecool/controller/LogInController.java
@@ -1,4 +1,21 @@
 package com.codecool.controller;
 
+import com.codecool.model.User;
+
 public class LogInController {
+    public boolean validateUserAccount(String logIn, String password, String type) {
+        User user = findUser(logIn);
+
+        if (user == null) {return false;}
+
+        if (!user.getPassword().equals(password)) {return false;}
+
+        if (!user.getType().equals(type)) {return false;}
+
+        return true;
+    }
+
+    private User findUser(String logIn) {
+        return null;
+    }
 }
