1caacd386c6902cd6870c5f7c8d162682980596a
piotrkaminski95 <pkaminki95@gmail.com>
2018-05-16 14:21:45 +0200

    implement getDoneAssignments() & getUnDoneAssignments()
    merge development

--- ccms/src/main/java/com/codecool/model/Student.java
+++ ccms/src/main/java/com/codecool/model/Student.java
@@ -4,22 +4,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Student extends User{
-    final static String type = "student";
-    List<Assignment> assigments;
+    private final static String type = "student";
+    private List<Assignment> assigments;
 
-    public Student(String id, String password, String name, String surname, String eMail) {
-        super(id, password, name, surname, eMail, type);
+    public Student(String logIn, String password, String name, String surname, String eMail) {
+        super(logIn, password, name, surname, eMail, type);
         assigments = new ArrayList<Assignment>();
     }
 
     public String[] getContactData() {
-        String[] result = new String[] {
+        return new String[] {
                 this.getName(),
                 this.getSurname(),
                 this.geteMail()
         };
-        return result;
     }
 
+    public List<Assignment> getDoneAssignment() {
+        List<Assignment> doneAssigments = new ArrayList<Assignment>();
 
+        for (Assignment assignment: assigments) {
+            if (assignment.isDone()) {
+                doneAssigments.add(assignment);
+            }
+        }
+        return doneAssigments;
+    }
+
+    public List<Assignment> getUnDoneAssignment() {
+        List<Assignment> unDoneAssignments = new ArrayList<Assignment>();
+
+        for (Assignment assignment: assigments) {
+            if (!assignment.isDone()) {
+                unDoneAssignments.add(assignment);
+            }
+        }
+        return unDoneAssignments;
+    }
+
+    public List<Assignment> getAssigments() {
+        return assigments;
+    }
 }
