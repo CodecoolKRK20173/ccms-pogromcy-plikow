3bee0e45c92a2b0f0339cb7c179cd23b09bf2d48
piotrkaminski95 <pkaminki95@gmail.com>
2018-05-16 12:56:39 +0200

    implement constructor & getContactData()

--- ccms/src/main/java/com/codecool/model/Student.java
+++ ccms/src/main/java/com/codecool/model/Student.java
@@ -1,4 +1,25 @@
 package com.codecool.model;
 
-public class Student {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student extends User{
+    final static String type = "student";
+    List<Assignment> assigments;
+
+    public Student(String id, String password, String name, String surname, String eMail) {
+        super(id, password, name, surname, eMail, type);
+        assigments = new ArrayList<Assignment>();
+    }
+
+    public String[] getContactData() {
+        String[] result = new String[] {
+                this.getName(),
+                this.getSurname(),
+                this.geteMail()
+        };
+        return result;
+    }
+
+
 }
