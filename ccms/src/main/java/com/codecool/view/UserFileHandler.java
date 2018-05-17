3b37aef9ade739126d624b779e492dbe93aa268d
piotrkaminski95 <pkaminki95@gmail.com>
2018-05-17 15:58:40 +0200

    add UserFileHandler implement saveUsers()

--- ccms/src/main/java/com/codecool/view/UserFileHandler.java
+++ ccms/src/main/java/com/codecool/view/UserFileHandler.java
@@ -1,6 +1,7 @@
 package com.codecool.view;
 
 import com.codecool.model.DataContainer;
+import com.codecool.model.Student;
 import com.codecool.model.User;
 
 import java.io.*;
@@ -15,7 +16,61 @@ public class UserFileHandler {
     }
 
     public void loadUsers() {
+        String line;
+        String separator = ",";
 
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filePath));
+
+            while ((line = br.readLine()) != null) {
+                String[] information = line.split(separator);
+                addUser(information);
+            }
+        } catch (IOException e) {
+            System.out.println("File not found");
+        }
+    }
+
+    private void addUser(String[] information) {
+        int logIn = 0;
+        int password = 1;
+        int type = 2;
+        int name = 3;
+        int surName = 4;
+        int eMail = 5;
+
+        if (information[type].equals("manager")) {
+            dataContainer.addManager(
+                    information[logIn],
+                    information[password],
+                    information[name],
+                    information[surName],
+                    information[eMail]);
+        } else if (information[type].equals("mentor")){
+            dataContainer.addStudent(
+                    information[logIn],
+                    information[password],
+                    information[name],
+                    information[surName],
+                    information[eMail]);
+
+        }else if (information[type].equals("student")){
+            dataContainer.addStudent(
+                    information[logIn],
+                    information[password],
+                    information[name],
+                    information[surName],
+                    information[eMail]);
+
+        } else {
+                dataContainer.addRegularEmployee(
+                        information[logIn],
+                        information[password],
+                        information[name],
+                        information[surName],
+                        information[eMail]);
+
+            }
     }
 
     public void saveUsers() {
