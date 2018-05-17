12f1ff39c05a32eed421f5ebb67e1ac90c03ac10
Czakero <bartosz.pyrz96@gmail.com>
2018-05-17 10:31:20 +0200

    Extend data container class

--- ccms/src/main/java/com/codecool/Application.java
+++ ccms/src/main/java/com/codecool/Application.java
@@ -1,13 +1,17 @@
 package com.codecool;
 
+import com.codecool.view.LogInMenu;
+import com.codecool.view.Menu;
+
 /**
  * Hello world!
  *
  */
 public class Application
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+        Menu menu = new LogInMenu();
+        menu.run();
     }
 }
--- ccms/src/main/java/com/codecool/model/DataContainer.java
+++ ccms/src/main/java/com/codecool/model/DataContainer.java
@@ -5,20 +5,20 @@ import java.util.List;
 
 public class DataContainer {
     private static final String MENTOR = "mentor";
-    private List<User> users;
+    private static final String REGULAR_EMPLOYEE = "regular employee";
+    private static final String MANAGER = "manager";
     private List<User> students;
     private List<User> mentors;
+    private List<User> managers;
+    private List<User> regularEmployees;
     private List<Assignment> assignments;
 
     public DataContainer() {
-        this.users = new ArrayList<User>();
         this.students = new ArrayList<User>();
         this.mentors = new ArrayList<User>();
         this.assignments = new ArrayList<Assignment>();
-    }
-
-    public List<User> getUsers() {
-        return users;
+        this.managers = new ArrayList<User>();
+        this.regularEmployees = new ArrayList<User>();
     }
 
     public List<User> getStudents() {
@@ -32,28 +32,40 @@ public class DataContainer {
     public List<Assignment> getAssignments() {
         return assignments;
     }
-    public void addUser(User user) {
-        users.add(user);
-    }
     public void addStudent(String login, String password, String name, String surname, String eMail) {
         User student = new Student(login, password, name, surname, eMail);
         students.add(student);
-        addUser(student);
     }
     public void addMentor(String login, String password, String name, String surname, String eMail) {
         User mentor = new User(login, password, name, surname, eMail, MENTOR);
         mentors.add(mentor);
-        addUser(mentor);
     }
     public void deleteStudent(User student) {
         students.remove(student);
-        deleteUser(student);
     }
     public void deleteMentor(User mentor) {
         mentors.remove(mentor);
-        deleteUser(mentor);
     }
-    public void deleteUser(User user) {
-        users.remove(user);
+    public void addManager(String login, String password, String name, String surname, String eMail) {
+        User manager = new User(login, password, name, surname, eMail, MANAGER);
+        managers.add(manager);
+    }
+
+    public void deleteManager(User manager) {
+        managers.remove(manager);
+    }
+    public void addRegularEmployee(String login, String password, String name, String surname, String eMail) {
+        User regularEmployee = new User(login, password, name, surname, eMail, REGULAR_EMPLOYEE);
+        regularEmployees.add(regularEmployee);
+    }
+    public void deleteRegularEmployee(User regularEmployee) {
+        regularEmployees.remove(regularEmployee);
+    }
+    public List<User> getManagers() {
+        return managers;
+    }
+
+    public List<User> getRegularEmployees() {
+        return regularEmployees;
     }
 }
