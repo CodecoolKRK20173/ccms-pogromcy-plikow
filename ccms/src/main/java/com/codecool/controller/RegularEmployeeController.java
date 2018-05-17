b726a6dd6dbc366958c96973617b35d1cd8df0e3
Czakero <bartosz.pyrz96@gmail.com>
2018-05-17 13:50:37 +0200

    Implement regular employee controller class

--- ccms/src/main/java/com/codecool/controller/RegularEmployeeController.java
+++ ccms/src/main/java/com/codecool/controller/RegularEmployeeController.java
@@ -1,4 +1,20 @@
 package com.codecool.controller;
 
+import com.codecool.model.DataContainer;
+import com.codecool.model.User;
+
+import java.util.List;
+
 public class RegularEmployeeController {
+
+    private DataContainer data;
+
+    public RegularEmployeeController() {
+        this.data = DataContainer.getInstance();
+
+    }
+    public List<User> getStudents() {
+        return data.getStudents();
+
+    }
 }
