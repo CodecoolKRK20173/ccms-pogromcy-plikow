eba39f7c3d0cedd2e7b72bf06b7034e25e001e4c
kret0 <mjp.sik@gmail.com>
2018-06-01 05:10:24 +0200

    Create DatabaseCreator app skelet

---
+++ ccms/src/main/java/com/codecool/DatabaseCreator.java
@@ -0,0 +1,12 @@
+package com.codecool;
+
+import com.codecool.dao.UsersDAO;
+import com.codecool.model.User;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseCreator {}}
+}
